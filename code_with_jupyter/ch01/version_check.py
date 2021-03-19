12e387094a3d6134225911a3491d6ebdc2cafa00
LDJWJ <frontier1020@naver.com>
2021-03-19 16:16:43 +0900

    20210319_update

---
+++ code_with_jupyter/ch01/version_check.py
@@ -0,0 +1,21 @@
+import sys
+import pandas as pd
+import matplotlib as mpl
+import numpy as np
+import sklearn as sk
+import tensorflow as tf
+import keras
+import xgboost as xgb
+import lightgbm as lgb
+import hyperopt
+
+print("파이썬 버전 : ", sys.version)
+print("팬더스 버전 : ", pd.__version__)
+print("matplotlib 버전 : ", mpl.__version__)
+print("넘파이 버전 : ", np.__version__)
+print("scikit-learn 버전 : ", sk.__version__)
+print("tensorflow 버전 : ", tf.__version__)
+print("케라스 버전 : ", keras.__version__)
+print("xgboost 버전 : ", xgb.__version__)
+print("lightgbm 버전 : ", lgb.__version__)
+print("hyperopt 버전 : ", hyperopt.__version__)
